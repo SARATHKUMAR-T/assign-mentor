2479120a515d80d86f4d1d7918fbdc84733ea0a7
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-06-06 15:45:54 +0530

    change

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-import  express  from "express";
-import dotenv from "dotenv"
-import 'dotenv/config'
+import express from "express";
+import dotenv from "dotenv";
+import "dotenv/config";
 import dbConnect from "./db.js";
 import { studentRouter } from "./Routes/studentRoute.js";
 import { mentorRouter } from "./Routes/mentorRoute.js";
-
+import { student } from "./Models/student.js";
 
 // express intialization
-const app=express()
-
+const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 // dbConnection
 dbConnect();
 
-
 // student router
-app.use("/api",studentRouter)
+app.use("/api", studentRouter);
 
 // mentor router
-app.use("/api",mentorRouter)
+app.use("/api", mentorRouter);
 
-// 
-app.use('/',(req,res)=>{
-    try {
-        res.status(200).send("welcome to the website")
-    } catch (error) {
-        
-    }
-})
-
+//
+app.use("/", async (req, res) => {
+  try {
+    const data = await student.find();
+    res.status(200).json({ message: "data loaded successfully", data: data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 // listening to port
-app.listen(process.env.PORT,()=>console.log('server started successfully'))
-
+app.listen(process.env.PORT, () => console.log("server started successfully"));
